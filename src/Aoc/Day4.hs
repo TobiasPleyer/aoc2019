07bc4c6e094ce206d9f88f39518194a20c82de0d
Tobias Pleyer <Tobias.Pleyer@mixed-mode.de>
2019-12-04 12:02:05 +0100

    Day 4 - part 1

--- src/Aoc/Day4.hs
+++ src/Aoc/Day4.hs
@@ -1,9 +1,10 @@
-module Aoc.Day4
-    ( solution
-    ) where
+module Aoc.Day4 where
+--    ( solution
+--    ) where
 
 
 import Control.Arrow ((***))
+import Data.Digits (digits)
 import Data.List (break)
 
 import Aoc.Common (DailyChallenge(..), isDebug)
@@ -14,8 +15,43 @@ getInput
 getInput = ((read *** (read . tail)) . break (=='-')) <$> readFile "inputs/day4.txt"
 
 
+getDigits = digits 10
+
+
+checkCriteria :: [(Int -> Bool)] -> Int -> Bool
+checkCriteria criteria number = and $ map ($ number) criteria
+
+
+isSixDigit :: Int -> Bool
+isSixDigit n = (n>=100000) && (n <= 999999)
+
+
+withinRange :: Int -> Int -> Int -> Bool
+withinRange lower upper n = (lower <= n) && (n <= upper)
+
+
+hasAdjacentDigits :: Int -> Bool
+hasAdjacentDigits n
+  | n < 10 = False
+  | otherwise = any (uncurry (==)) (zip (init ds) (tail ds))
+  where ds = getDigits n
+
+
+digitsDontDecrease :: Int -> Bool
+digitsDontDecrease n = all (\(a,b) -> a <= b) (zip (init ds) (tail ds))
+  where ds = getDigits n
+
+
 --solution_p1 :: ([Direction Int],[Direction Int]) -> IO String
-solution_p1 = return ""
+solution_p1 (start,end) = do
+  let
+    criteriaOk = checkCriteria [
+                   isSixDigit,
+                   hasAdjacentDigits,
+                   digitsDontDecrease]
+    passwords = [p | p <- [start..end], criteriaOk p]
+    passwordCount = length passwords
+  return $ show passwordCount
 
 
 --solution_p2 :: ([Direction Int],[Direction Int]) -> IO String
@@ -26,4 +62,4 @@ solution :: IO DailyChallenge
 solution = do
   input <- getInput
   --print input
-  return $ DailyChallenge (solution_p1 ) Nothing
+  return $ DailyChallenge (solution_p1 input) Nothing
