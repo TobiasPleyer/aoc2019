eceff2e348a75a386933084590c2ef1510262c60
Tobias Pleyer <Tobias.Pleyer@mixed-mode.de>
2019-12-04 12:47:12 +0100

    Day 4 - part 2

--- src/Aoc/Day4.hs
+++ src/Aoc/Day4.hs
@@ -5,7 +5,7 @@ module Aoc.Day4 where
 
 import Control.Arrow ((***))
 import Data.Digits (digits)
-import Data.List (break)
+import Data.List (break, group)
 
 import Aoc.Common (DailyChallenge(..), isDebug)
 
@@ -31,10 +31,11 @@ withinRange lower upper n = (lower <= n) && (n <= upper)
 
 
 hasAdjacentDigits :: Int -> Bool
-hasAdjacentDigits n
-  | n < 10 = False
-  | otherwise = any (uncurry (==)) (zip (init ds) (tail ds))
-  where ds = getDigits n
+hasAdjacentDigits n = any ((>=2) . length) $ group (getDigits n)
+
+
+hasExactlyTwoAdjacentDigits :: Int -> Bool
+hasExactlyTwoAdjacentDigits n = any ((==2) . length) $ group (getDigits n)
 
 
 digitsDontDecrease :: Int -> Bool
@@ -42,7 +43,7 @@ digitsDontDecrease n = all (\(a,b) -> a <= b) (zip (init ds) (tail ds))
   where ds = getDigits n
 
 
---solution_p1 :: ([Direction Int],[Direction Int]) -> IO String
+solution_p1 :: (Int,Int) -> IO String
 solution_p1 (start,end) = do
   let
     criteriaOk = checkCriteria [
@@ -54,12 +55,20 @@ solution_p1 (start,end) = do
   return $ show passwordCount
 
 
---solution_p2 :: ([Direction Int],[Direction Int]) -> IO String
---solution_p2 = return ""
+solution_p2 :: (Int,Int) -> IO String
+solution_p2 (start,end) = do
+  let
+    criteriaOk = checkCriteria [
+                   isSixDigit,
+                   hasExactlyTwoAdjacentDigits,
+                   digitsDontDecrease]
+    passwords = [p | p <- [start..end], criteriaOk p]
+    passwordCount = length passwords
+  return $ show passwordCount
 
 
 solution :: IO DailyChallenge
 solution = do
   input <- getInput
   --print input
-  return $ DailyChallenge (solution_p1 input) Nothing
+  return $ DailyChallenge (solution_p1 input) (Just (solution_p2 input))
